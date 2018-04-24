5308e25c58cc99f4f7e8eb0f473883ce97d8f016
Sushil Dangi <dangi.sushil5@gmail.com>
2018-04-24 22:56:09 +0530

    Template Inheritance Done

--- posts/views.py
+++ posts/views.py
@@ -13,7 +13,7 @@ def post_list(request):
         "object_list": queryset,
         "title": "List"
     }
-    return render(request, "index.html", context)
+    return render(request, "post_list.html", context)
 
 
 def post_create(request):
@@ -23,8 +23,6 @@ def post_create(request):
         instance.save()
         messages.success(request, "Successfully Created!")
         return HttpResponseRedirect(instance.get_absolute_url())
-    else:
-        messages.error(request, "Not Successfully Created!")
     context = {
         "title": "Create",
         "form": form
