edf66ef6bf1ee421c64b886ced2e5539b08068e7
Sushil Dangi <dangi.sushil5@gmail.com>
2018-04-26 23:25:12 +0530

    Slug Field Added

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -9,6 +11,7 @@ def upload_location(instance, filename):
 
 class Post(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField(unique=True)
     image = models.ImageField(null=True, blank=True,
                               height_field="height_field",
                               width_field="width_field",
@@ -25,7 +28,27 @@ class Post(models.Model):
     def get_absolute_url(self):
         # return "/posts/%s/" % (self.id)
 
-        return reverse('posts:detail', kwargs={"id": self.id})
+        return reverse('posts:detail', kwargs={"slug": self.slug})
 
     class Meta:
         ordering = ['-timestamp', '-updated']
+
+
+def create_slug(instance, new_slug=None):
+    slug = slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    qs = Post.objects.filter(slug=slug).order_by("-id")
+    exists = qs.exists()
+    if exists:
+        new_slug = "%s-%s" % (slug, qs.first().id)
+        return create_slug(instance, new_slug=new_slug)
+    return slug
+
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
